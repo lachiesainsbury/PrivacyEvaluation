52f55f4e99e3b9affd9b9d31ba7a4e3a74fb53a4
lachiesainsbury <lachies.spare@gmail.com>
2018-10-15 14:22:21 +1100

    Pushing to laptop

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import scanner
+import utility.genILoss as gl
 
 if __name__ == '__main__':
 
     x, y = scanner.readData("data/adult.csv")
     print(x[0], y[0])
+
+    gl.calcGenILoss(x, y)
---
+++ utility/genILoss.py
@@ -0,0 +1,10 @@
+import scanner
+
+
+def calcGenILoss(x, y) :
+    recordCount = len(x)
+    attributeCount = len(x[0])
+
+    print(recordCount, attributeCount)
+
+
