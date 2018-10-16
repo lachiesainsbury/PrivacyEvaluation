c27a582d14bc5f751f616db5d9aa24e9d042fbab
Mitch\Mitch <mitchellburley0@gmail.com>
2018-10-16 13:50:53 +1100

    refactored aecsm file + added to main

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import scanner
 import utility.genILoss as gl
+import utility.aecsm as aecsm
+
 
 if __name__ == '__main__':
 
     x, y = scanner.readData("data/adult.csv")
-    print(x[0], y[0])
+    # print(x[0], y[0])
 
     gl.calcGenILoss(x, y)
+    aecsmresult = aecsm.calculateAECSM()
+    print(aecsmresult)
