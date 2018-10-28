48fc1276a720c80ca3eff65cc4680cb749d43e01
lachiesainsbury <lachies.spare@gmail.com>
2018-10-28 21:09:37 +1100

    Adding 75-bike-sharing

--- graph.py
+++ graph.py
@@ -6,3 +6,13 @@ def plot(x, y, title, xlabel, ylabel):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.show()
+
+
+def plotTwo(x, y1, y2, title, xlabel, ylabel):
+    plt.plot(x, y1, linestyle='--', marker='o')
+    plt.plot(x, y2, linestyle='--', marker='o')
+    plt.title(title)
+    plt.legend(['Adult', 'Bike Sharing'], loc='lower right')
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.show()
